13543370087bf8b3707c48bf804348e7c451a822
Sam Shelton <sam.shelton@soloworks.co.uk>
2019-05-17 16:45:30 +0100

    Expanded Debugging

--- compileAll.ps1
+++ compileAll.ps1
@@ -9,7 +9,9 @@ $SPlusCC = "C:\Program Files (x86)\Crestron\Simpl\SPlusCC.exe"
 # Loop through all .usp files in modules folder
 Get-ChildItem (Resolve-Path -Path ".\SIMPLPlusModules\").Path -Filter *.usp | 
 Foreach-Object {
-    Write-Host -NoNewline "Found Module `"$($_.Name)`"..."
+    
+    Write-Host "Processing Module `"$($_.Name)`""
+
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
     $pinfo.FileName = $SPlusCC
     $pinfo.RedirectStandardError = $true
@@ -19,17 +21,19 @@ Foreach-Object {
     $p = New-Object System.Diagnostics.Process
     $p.StartInfo = $pinfo
 
-    Write-Host -NoNewline "Compiling..."
+    Write-Host "Calling Compiler:"
+    Write-Host "EXE: $($SPlusCC)"
+    Write-Host "ARG: $($pinfo.Arguments)"
     $p.Start() | Out-Null
     $p.WaitForExit()
     
-    #$stdout = $p.StandardOutput.ReadToEnd()
+    $stdout = $p.StandardOutput.ReadToEnd()
     $stderr = $p.StandardError.ReadToEnd()
 
     if($p.ExitCode -eq 0){
         $Duration = New-TimeSpan -Start $p.StartTime -End $p.ExitTime
         Write-Host "Success ($($Duration.Seconds)s)"
-        #Write-Host "stdout: $stdout"
+        Write-Host "stdout: $stdout"
     }else{
         Write-Host "Error (ExitCode:$($p.ExitCode))"
         Write-Host "stderr: $stderr"
